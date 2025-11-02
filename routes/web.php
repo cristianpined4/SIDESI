9ee9cd5b4d4da48ce952d3168958dd8f3c028a43
Cristian Pineda <cristianpined4@outlook.com>
2025-11-01 23:32:24 -0600

    fixed bugs

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ Route::get('/contactos', App\Livewire\Site\ContactoController::class)->name('sit
 Route::get('/documentos', App\Livewire\Site\DocumentosController::class)->name('site.documentos');
 Route::get('/eventos', App\Livewire\Site\EventosController::class)->name('site.eventos');
 Route::get('/perfil', App\Livewire\PerfilController::class)->name('profile');
+Route::get('/ofertas', App\Livewire\Site\Ofertas_EmpleoController::class)->name('site.ofertas');
 Route::get('/certificado/{code}', function ($code) {
   return "Certificado {$code}";
 })->name('ver-certificado');
